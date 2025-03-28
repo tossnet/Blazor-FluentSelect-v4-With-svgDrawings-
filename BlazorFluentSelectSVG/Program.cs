2daa4ad5d562fbc130be2a2d64cf5b43c09ab4e7
Christophe Peugnet <christophe.peugnet@outlook.fr>
2025-03-28 08:57:38 +0100

    .

--- BlazorFluentSelectSVG/Program.cs
+++ BlazorFluentSelectSVG/Program.cs
@@ -1,10 +1,13 @@
 using BlazorFluentSelectSVG.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddFluentUIComponents();
 
 var app = builder.Build();
 
